9731636756784c87fc92ea3e0eca2a2d36181734
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-09 22:45:34 -0300

    Implementando construtor e subimssão do form

--- frontend/src/app/pessoa-form/pessoa-form.component.ts
+++ frontend/src/app/pessoa-form/pessoa-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PessoaService } from '../pessoa.service';
+import { Pessoa } from '../pessoa.interface';
 
 @Component({
   selector: 'app-pessoa-form',
@@ -8,5 +11,32 @@ import { Component } from '@angular/core';
   styleUrl: './pessoa-form.component.css'
 })
 export class PessoaFormComponent {
+  pessoaForm: FormGroup;
+  pessoas: Pessoa[] = [];
+  selectedPessoa: Pessoa | null = null;
+
+  constructor(private formBuilder: FormBuilder, private pessoaService: PessoaService) {
+    this.pessoaForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      data_nasc: ['', Validators.required],
+      cpf: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+      sexo: ['', Validators.required],
+      altura: ['', [Validators.required, Validators.min(0)]],
+      peso: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.pessoaForm.valid) {
+      const pessoa: Pessoa = this.pessoaForm.value;
+      this.pessoaService.create(pessoa).subscribe(
+        response => {
+          console.log('Pessoa criada com sucesso!', response);
+          this.pessoaForm.reset();
+        },
+        error => console.error('Erro ao criar pessoa', error)
+      );
+    }
+  }
 
 }
